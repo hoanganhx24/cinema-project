2f69a095aff936ca87d9f533a18eb889d72b07a8
Phạm Như Hoàng <phamnhuhoang24102004@gmail.com>
2026-04-01 23:34:22 +0700

    first commit

---
+++ services/booking-service/src/test/java/com/hoanganh24/bookingservice/BookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoanganh24.bookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ services/logging-service/src/test/java/com/hoanganh24/loggingservice/LoggingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoanganh24.loggingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
